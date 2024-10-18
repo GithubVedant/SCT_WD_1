4d62b5b3c69e4e1b6071dd08f32a4180e9093474
RAJESHIRKE VEDANT PRAKASH <145147865+GithubVedant@users.noreply.github.com>
2024-10-18 23:07:48 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// Change background color of navbar when scrolling
+window.addEventListener('scroll', function() {
+    const navbar = document.getElementById('navbar');
+    if (window.scrollY > 100) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
